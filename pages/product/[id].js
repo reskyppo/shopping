2a1d0f6ec8814d23876780c6d631a8e8d7b6e36a
Reskyppo <reskybeen@gmail.com>
2021-05-25 21:43:33 +0700

    wip: dynamic product page

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -1,12 +1,19 @@
 import { useRouter } from "next/router";
 
+import Carousel from "../../src/components/Carousel";
+import { Products } from "../../src/utils/data";
+
 const Product = () => {
   const router = useRouter();
   const { id } = router.query;
+
+  const data = Products.filter((product) => product.id == id);
+  
   return (
-    <div>
-      <h1>{id}</h1>
-    </div>
+    <p>
+      {data[0].name}
+      {data[0].price}
+    </p>
   );
 };
 
