0eced7b984c4b3858d37d7ba85717fc34949607c
Reskyppo <reskybeen@gmail.com>
2021-05-25 11:24:21 +0700

    create dummy data

---
+++ src/utils/data.js
@@ -0,0 +1,16 @@
+const Products = [
+  {
+    id: 1,
+    name: "tas",
+    price: "100.000",
+    img: "https://images.unsplash.com/photo-1598532163257-ae3c6b2524b6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80",
+  },
+  {
+    id: 2,
+    name: "tas",
+    price: "200.000",
+    img: "https://images.unsplash.com/photo-1598532163257-ae3c6b2524b6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80",
+  },
+];
+
+export { Products };
